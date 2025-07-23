7971d13575edde80ed021dd6374751e4ffa3f29c
AlvinIsCrack <animations468@gmail.com>
2025-07-23 16:41:38 -0400

    w

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const dev = process.argv.includes('dev');
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 	kit: {
-		pages: 'build',
-		assets: 'build',
-		fallback: undefined,
-		precompress: false,
-		strict: true,
 		adapter: adapter(),
 		paths: {
-			base: dev ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
